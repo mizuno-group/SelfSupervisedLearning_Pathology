1626c449162ecadd066fc3f9f43670dc057846ae
KatsuhisaMorita <winwinwin19963rd@gmail.com>
2024-01-17 18:05:02 +0900

    [240117morita] mil featurize module

--- mil/featurize.py
+++ mil/featurize.py
@@ -16,6 +16,8 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 from PIL import ImageOps, Image
+import cv2
+from openslide import OpenSlide
 
 import sslmodel
 import sslmodel.sslutils as sslutils
@@ -26,6 +28,7 @@ class Featurize:
         self.DEVICE=DEVICE
         self.lst_size=lst_size
         self.out_all=[[] for size in self.lst_size]
+        self.out_all_pool=[[] for size in self.lst_size]
 
     def extraction():
         return None
@@ -39,11 +42,25 @@ class Featurize:
                 for i, out in enumerate(outs):
                     self.out_all[i].append(out)
 
+    def pooling(self, num_patch:int=200):
+        """max pooling"""
+        for i, out in enumerate(self.out_all):
+            self.out_all_pool[i].append(
+                np.max(out.reshape(-1, num_patch, self.lst_size[i]),axis=1)
+                )
+        # reset output list
+        self.out_all=[[] for size in self.lst_size]
+
     def save_outall(self, folder="", name=""):
         for i, out in enumerate(self.out_all):
             out=np.concatenate(out).astype(np.float32)
             np.save(f"{folder}/{name}_layer{i+1}.npy", out)
         self.out_all=[np.zeros((0,size), dtype=np.float32) for size in self.lst_size]
+
+    def save_outpool(self, folder="", name=""):
+        for i, out in enumerate(self.out_all_pool):
+            out=np.concatenate(out).astype(np.float32)
+            np.save(f"{folder}/{name}_layer{i+1}.npy", out)
 
 class ResNet18Featurize(Featurize):
     def __init__(self, DEVICE="cpu"):
@@ -195,33 +212,102 @@ def _load_state_dict_dense(model, weights):
     return model
 
 # DataLoader
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self,
-                filein:str="",
-                transform=None,
-                ):
+class DatasetWSI(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        filein:str="",
+        transform=None,
+        patch_size=224,
+        num_patch=None, random_state=24771,
+        ):
         # set transform
         if type(transform)!=list:
             self._transform = [transform]
         else:
             self._transform = transform
         # load data
-        with open(filein, 'rb') as f:
-            self.data = np.load(f)
-        self.datanum = len(self.data)
+        self.wsi = OpenSlide(filein)
+        mask_inside=get_mask_inside(self.wsi, patch_size=patch_size)
+        lst_number=np.array(range(len(mask_inside.flatten())))[mask_inside.flatten()]
+        self.lst_location=[[int(patch_size*v) for v in divmod(number, mask_inside.shape[1])] for number in lst_number]
+        if num_patch:
+            random.seed(random_state)
+            self.lst_location=random.sample(self.lst_location, num_patch)
+        self.datanum = len(self.lst_location)
+        self.patch_size=patch_size
 
     def __len__(self):
         return self.datanum
 
     def __getitem__(self,idx):
-        out_data = self.data[idx]
+        out_data=self.wsi.read_region(
+            location=tuple(self.lst_location[idx]),
+            level=0,
+            size=(self.patch_size, self.patch_size),
+            )
+        out_data = np.array(out_data, np.uint8)[:,:,:3]
         out_data = Image.fromarray(out_data).convert("RGB")
         if self._transform:
             for t in self._transform:
                 out_data = t(out_data)
         return out_data
 
-def prepare_dataset(filein:str="", batch_size:int=32):
+def get_mask_inside(
+    image, 
+    patch_size:int=224,
+    slice_min_patch:int=1000,
+    adjacent_cells_slice=[(-1,0),(1,0),(0,-1),(0,1),],
+    adjacent_cells_inside=[(-1,0),(1,0),(0,-1),(0,1),(1,1),(1,-1),(-1,1),(-1,-1)],
+    ):
+    # load
+    level = image.get_best_level_for_downsample(patch_size)
+    downsample = image.level_downsamples[level]
+    ratio = patch_size / downsample
+    whole = image.read_region(location=(0,0), level=level,
+        size = image.level_dimensions[level]).convert('HSV')
+    whole = whole.resize((int(whole.width / ratio), int(whole.height / ratio)))
+    whole = np.array(whole, dtype=np.uint8)
+    # get mask
+    saturation = whole[:,:,1] #saturation
+    threshold, _ = cv2.threshold(saturation, 0, 255, cv2.THRESH_OTSU)
+    mask = saturation > threshold
+    # get slices > slice_min_patch
+    left_mask = np.full((mask.shape[0]+1, mask.shape[1]+1), fill_value=False, dtype=bool)
+    left_mask[:-1, :-1] = mask
+    n_left_mask = np.sum(left_mask)
+    slice_idx = np.full_like(mask, fill_value=-1, dtype=int)
+    i_slice = 0
+    while n_left_mask > 0:
+        mask_i = np.where(left_mask)
+        slice_left_indices = [(mask_i[0][0], mask_i[1][0])]
+        while len(slice_left_indices) > 0:
+            i, j = slice_left_indices.pop()
+            if left_mask[i, j]:
+                slice_idx[i, j] = i_slice
+                for adj_i, adj_j in adjacent_cells_slice:
+                    if left_mask[i+adj_i, j+adj_j]:
+                        slice_left_indices.append((i+adj_i, j+adj_j))
+                left_mask[i, j] = False
+                n_left_mask -= 1
+        slice_mask = slice_idx == i_slice
+        if np.sum(slice_mask) < slice_min_patch:
+            slice_idx[slice_mask] = -1
+        else:
+            i_slice += 1
+    # re-difinition mask
+    mask=slice_idx>-1
+    # get inside mask
+    mask_inside=np.zeros(mask.shape, dtype=bool)
+    for i in range(1,mask.shape[0]-1):
+        for v in range(1,mask.shape[1]-1):
+            tf = mask[i][v]
+            if tf:
+                for adj in adjacent_cells_inside:
+                    tf&=mask[i+adj[0]][v+adj[1]]
+            mask_inside[i][v]=tf
+    return mask_inside
+
+def prepare_dataset(filein:str="", batch_size:int=32, num_patch=None,):
     """
     data preparation
     
@@ -235,16 +321,17 @@ def prepare_dataset(filein:str="", batch_size:int=32):
         normalize
     ])
     # data
-    dataset = Dataset(
+    dataset = DatasetWSI(
         filein=filein,
         transform=data_transform,
+        num_patch=num_patch,
         )
     # to loader
     data_loader = sslmodel.data_handler.prep_dataloader(
         dataset, batch_size, 
         shuffle=False,
         drop_last=False)
-    return data_loader
+    return data_loader, dataset.lst_location
 
 ## Featurize Methods
 # name: [Model_Class, last_layer_size, Featurize_Class]
@@ -331,4 +418,4 @@ def featurize_layer(
         data_loader=prepare_dataset(filein=filein, batch_size=batch_size)
         extract_class.featurize(model, data_loader)
         extract_class.save_outpool(folder=folder_name, name=result_name)
-
+        
--- tggate/utils.py
+++ tggate/utils.py
@@ -19,19 +19,14 @@ def check_file(str_file):
 
 def get_patch_mask(image, patch_size, threshold=None,):
     """
-    get_patchesに加え, WSI内に複数ある切片を分けて認識するようにする。
-
     Parameters
     ----------
     iamge: openslide.OpenSlide
     patch_size: int
-    threshold: float or None
-        各patchが背景かどうかを判定する彩度のthreshold.
-        Noneの場合, OTSU法で決定する。
+    threshold: float or None (OTSU)
     Returns
     -------
     mask: np.array(int)[wsi_height, wsi_width]
-        各patchがどの切片に属するか(-1=背景)
     """
     level = image.get_best_level_for_downsample(patch_size)
     downsample = image.level_downsamples[level]
@@ -41,7 +36,6 @@ def get_patch_mask(image, patch_size, threshold=None,):
     whole = whole.resize((int(whole.width / ratio), int(whole.height / ratio)))
     whole = np.array(whole, dtype=np.uint8)
     saturation = whole[:,:,1]
-
     if threshold is None:
         threshold, _ = cv2.threshold(saturation, 0, 255, cv2.THRESH_OTSU)
     mask = saturation > threshold
