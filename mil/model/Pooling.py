4678fef0859773fdb706d0d86dd6201531499dbd
KatsuhisaMorita <winwinwin19963rd@gmail.com>
2024-01-17 09:56:58 +0900

    [240117 add PoolingMIL]

--- mil/model/Pooling.py
+++ mil/model/Pooling.py
@@ -1,41 +1,59 @@
 """ 
 Max Plooling and Logistic Regression for MIL
 """
-import numpy as np
 
-class MaxPooling:
-    def __init__(self, n_features, n_classes):
-        self.train_data=np.zeros((0,512), dtype=np.float32)
-        self.test_data=np.zeros((0,512), dtype=np.float32)
-    
-    def __call__(self, X):
-        return
+import random
+
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+class PoolingMIL:
+    def __init__(self, n_features:int=512, strategy="max"):
+        self.n_features=n_features
+        self.train_data=np.zeros((0,self.n_features), dtype=np.float32)
+        self.test_data=np.zeros((0,self.n_features), dtype=np.float32)
+        dict_strategy={
+            "max":self._max_pooling,
+            "min":self._min_pooling,
+            "mean":self._mean_pooling,
+        }
+        self.pooling=dict_strategy[strategy]
+        self.logistic_model=None
 
     def prepare_train_data(self, X):
+        self.train_data = self.pooling(self.train_data, X)    
 
     def prepare_test_data(self, X):
+        self.test_data = self.pooling(self.test_data, X)
 
+    def train(self, x_train=None, y_train=None, params=dict(), convertz=False,):
+        self.logistic_model=LogisticRegression(**params)
+        if x_train:
+            self.logistic_model.fit(x_train, y_train)
+        else:
+            self.logistic_model.fit(self.train_data, y_train)
 
-    def save_outall(self, folder="", name=""):
-            np.save(f"{folder}/{name}_layer{i+1}.npy", out)
+    def predict(self, x_test=None):
+        if x_test:
+            return self.logistic_model.predict_proba(x_test)[:,1]
+        else:
+            return self.logistic_model.predict_proba(self.test_data)[:,1]
 
-    def _max_pooling():
-        for i, out in enumerate(self.out_all):
-            self.out_all_pool[i] = np.concatenate([
-                self.out_all_pool[i],
-                self._pooling_array(out, num_patch=num_patch, size=self.lst_size[i])
-                ])
-        # reset output list
-        self.out_all=[np.zeros((0,size), dtype=np.float32) for size in self.lst_size]
+    def save_model(self, model_path=""):
+        pd.to_pickle(np.concatenate([
+            self.logistic_model.coef_,
+            self.logistic_model.intercept_,]),
+            model_path)
 
-    def _pooling_array(self, out,num_patch=64, size=256):
-        """ return max/min/mean pooling array with num_patch"""
-        out = out.reshape(-1, num_patch, size)
-        data_max=np.max(out,axis=1)
-        return data_all
+    def _max_pooling(self, data, X):
+        return np.concatenate([data, np.max(X, axis=1)])
 
-    def train():
-        return
+    def _min_pooling(self, data, X):
+        return np.concatenate([data, np.max(X, axis=1)])
 
-    def predict_proba():
-        return
+    def _mean_pooling(self, data, X):
+        return np.concatenate([data, np.max(X, axis=1)])
+
+    def _delete_data(self):
+        self.train_data=np.zeros((0,self.n_features), dtype=np.float32)
+        self.test_data=np.zeros((0,self.n_features), dtype=np.float32)
